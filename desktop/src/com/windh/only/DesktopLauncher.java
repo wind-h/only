5640ca7984a707d5e173552515e09fe1b493d97d
hsc <2387570738@qq.com>
2024-05-11 11:02:05 +0800

    init

--- desktop/src/com/windh/only/DesktopLauncher.java
+++ desktop/src/com/windh/only/DesktopLauncher.java
@@ -8,7 +8,7 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setForegroundFPS(60);
-		config.setTitle("onlyone");
+		config.setTitle("only");
 		new Lwjgl3Application(new Application(), config);
 	}
 }
